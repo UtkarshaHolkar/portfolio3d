b443f3c3730b228221b724845ccff0304d92e4c4
UtkashaHolakr <utkatshaaholar@gmail.com>
2023-07-29 17:11:31 +0530

    third commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,8 +25,8 @@ const Contact = () => {
     variants={slideIn('left', 'tween', 0.2, 1)}
     className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
     >
-    <p className={styles.sectionSubText}>Get in touch-utkarshaaholkar@gmail.com@gmail.com</p>
-    <h3 className={styles.sectionHeadText}>Contact-</h3>
+    <p className={styles.sectionSubText}>Get in touch-</p>
+    <h3 className={styles.sectionHeadText}>EMAIL</h3>
     <p className={styles.sectionSubText}></p>
     <form
     ref={formRef}
